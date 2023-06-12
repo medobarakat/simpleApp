3d0b451f6f05f3e41e12614dcb8b7cdee20666f0
medobarakat <ahmedbarakat2401@gmail.com>
2023-06-12 18:22:16 +0300

    finish the 2 screens

--- src/navigation/Auth.js
+++ src/navigation/Auth.js
@@ -1,23 +1,20 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/Home";
-import SignUp from "../screens/SignUp";
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import SignUp from '../screens/SignUp';
 // the screens
-
 
 const Stack = createNativeStackNavigator();
 
 const LoginNavigation = () => {
-    return (
-        <Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{ headerShown: false }}
-        >
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Signup" component={SignUp} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Signup" component={SignUp} />
+    </Stack.Navigator>
+  );
 };
 
 export default LoginNavigation;
-
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,51 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Box, Input} from 'native-base';
 import React from 'react';
-import Icon from "../icons/Icon"
-const Home = () => {
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
+
+const Home = ({navigation}) => {
+  const onPressHandler = () => {
+    navigation.navigate('Signup');
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.firstSection}></View>
-      <View style={styles.secondSection}>
-        <Icon />
+    <KeyboardAwareScrollView
+      contentContainerStyle={styles.container}
+      extraScrollHeight={-100}>
+      <View style={styles.firstSection}>
+        <View style={styles.iconWrapper}>
+          <Icon name="closecircleo" size={70} />
+        </View>
       </View>
-    </View>
+      <View style={styles.secondSection}>
+        <Text style={styles.secondSectiontxt}>App Board</Text>
+        <View>
+          <View>
+            <Box marginLeft={10} marginBottom={5}>
+              <Text>Quick Registration</Text>
+            </Box>
+            <Box alignItems="center">
+              <Input placeholder="Input" w="80%" />
+            </Box>
+          </View>
+          <View style={styles.btnContainer}>
+            <TouchableOpacity onPress={onPressHandler} style={styles.button}>
+              <Text style={styles.buttonText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   );
 };
 
@@ -16,14 +53,49 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
   },
   firstSection: {
     backgroundColor: '#111b31',
-    height: 300, 
+    height: 300,
     borderBottomRightRadius: 130,
-    borderBottomLeftRadius:130,
-    borderColor: '#111b31', 
+    borderBottomLeftRadius: 130,
+    borderColor: '#111b31',
+    position: 'relative',
+  },
+  iconWrapper: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    position: 'absolute',
+    bottom: -40,
+    left: '40%',
+  },
+  secondSection: {
+    marginTop: height / 12,
+  },
+  secondSectiontxt: {
+    textAlign: 'center',
+    marginBottom: height / 40,
+    fontSize: 24,
+  },
+  btnContainer: {
+    marginTop: height / 19,
+    width: width / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  button: {
+    backgroundColor: '#111b31',
+    width: 200,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
